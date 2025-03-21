f39cdec6eb858eed3029fce779c699287e2b0876
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-03-21 11:56:00 +0530

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
   
   return (
     <>
-     
+     <h1>image search engine</h1>
+     <form id="search-form">
+   <input type='text' id='search-box' placeholder='search any thing here ...'/>
+   <button>Search</button>
+     </form>
     
     </>
   )
